8940219ea3f15083f7225cb1884a1c1f33a2c055
Andrew Nichols <annich@microsoft.com>
2017-07-26 10:14:41 -0700

    Add Mobx and Office.js

--- src/test.tsx
+++ src/test.tsx
@@ -1,8 +1,35 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import {action, observable, computed} from "mobx";
+import {observer} from "mobx-react";
 
+class Info {
+    @observable private _userName:string;
+
+    @computed public get UserName():string { return this._userName; }
+
+    @action
+    public updateName(userName:string): void {
+        this._userName = userName;
+    }
+}
+
+let info: Info = new Info();
+
+Office.initialize = () => {
+    info.updateName(Office.context.mailbox.userProfile.displayName);
+}
+
+
+@observer
+class HelloWorld extends React.Component<{}, {}> {
+
+    public render(): JSX.Element {
+        return <div> Hello {info.UserName}! </div>;
+    }
+}
 
 ReactDOM.render(
-    (<div> Hello World! </div>),
+    (<HelloWorld />),
     document.getElementById("app")
 );
